104c47fda972079dd499f44ec6859ce490e7991c
99003679 <omisha.mahere@ltts.com>
2021-02-25 17:07:13 +0530

    Commit

--- ProblemThree_CompleteSolution_BL/GourmetCoffeeShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetCoffeeShop.cs
@@ -8,6 +8,15 @@ namespace ProblemThree_CompleteSolution_BL
 {
     public class GourmetCoffeeShop : GourmetShop, ITaxCompute
     {
-        
+        private int Gprod_id;
+        private string Gprod_name;
+        private Dictionary<int, double> coffeePriceDict;
+
+        public GourmetCoffeeShop(int c_id, string c_name, Dictionary<int, double> coffeePriceDict)
+        {
+            this.Gprod_id = c_id;
+            this.Gprod_name = c_name;
+            this.coffeePriceDict = coffeePriceDict;
+        }
     }
 }
--- ProblemThree_CompleteSolution_BL/GourmetOrganicShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetOrganicShop.cs
@@ -8,6 +8,23 @@ namespace ProblemThree_CompleteSolution_BL
 {
     public class GourmetOrganicShop : GourmetShop,ITaxCompute
     {
-        
+        public int Oprod_id;
+        public string Oprod_name;
+        private Dictionary<int, double> organicPriceDict;
+
+        public GourmetOrganicShop(int , string, )
+        {
+
+        }
+
+        public GourmetOrganicShop(int Gid, string Gname, Dictionary<int, double> organicPriceDict)
+        {
+            this.Oprod_id = Gid;
+            this.Oprod_name = Gname;
+            this.organicPriceDict = organicPriceDict;
+        }
+
+        public int ItemName { get; set; }
     }
+    
 }
